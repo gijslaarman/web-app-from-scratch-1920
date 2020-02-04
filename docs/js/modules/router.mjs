2f8f0289811943cec4be646d544c949a07de4b6d
gijslaarman <gijslaarman@live.nl>
2020-02-04 22:56:53 +0100

    added another fail safe for when users add / behind their urls

--- docs/js/modules/router.mjs
+++ docs/js/modules/router.mjs
@@ -7,7 +7,7 @@ export default class Router {
     }
 
     init(hash, callback) {
-        // Error handling
+        // Error handling for creating new routes
         if (!hash) throw new Error('No hash given')
         if (typeof hash !== "string") throw new TypeError('Hash has to be a string')
         if (!callback) throw new Error('No callback defined')
@@ -20,7 +20,7 @@ export default class Router {
 
         // All checks ok
         const route = {
-            hash: new RegExp("^#/" + hash.replace(/:[^\s/]+/g, '([\\w-]+)') + /* check if '/' is added behind + */ "$"), // replace the hash with a regex, that also transforms all :id params ":" with a RegEx that accepts all words, this RegEx is found on StackOverflow.
+            hash: new RegExp("^#/" + hash.replace(/:[^\s/]+/g, '([\\w-]+)') + "$"), // replace the hash with a regex, that also transforms all :id params ":" with a RegEx that accepts all words, this RegEx is found on StackOverflow.
             callback // callback: callback
         }
 
@@ -28,12 +28,19 @@ export default class Router {
     }
  
     view() {
-        if (window.location.hash) {
+        let urlHash = window.location.hash
+
+        if (urlHash.slice(-1) === '/') {
+            // If user puts an extra forward slash behind the url it will not be accounted for in the check.
+            urlHash = urlHash.slice(0, -1)
+        }
+
+        if (urlHash) {
             let hashFound
 
             this.routes.some(route => {
                 // If route.hash matches window.location.hash
-                if (window.location.hash.match(route.hash)) {
+                if (urlHash.match(route.hash)) {
                     hashFound = true
                     let req = {}
                     return route.callback.call(this, req)
